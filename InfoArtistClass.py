5368001b23ef35f72ab9521e0761f20b85203f44
100452234 <114150654+100452234@users.noreply.github.com>
2023-10-04 16:47:58 +0200

    upcoming concerts

--- InfoArtistClass.py
+++ InfoArtistClass.py
@@ -7,9 +7,10 @@ import sqlparse
 
 # Class to store information about one artist
 class InfoArtist:
-    def __init__(self, name="", wiki_report=""):
+    def __init__(self, name="", wiki_report="", concert_upcoming=[]):
         self.name = name
         self.wiki_report = wiki_report
+        self.concert_upcoming = concert_upcoming
 
     def set_name(self, name):
         self.name = name
@@ -19,42 +20,40 @@ class InfoArtist:
 
     def set_wiki_report(self):
         driver = webdriver.Chrome()
-        driver.get('https://www.google.com/')
+        driver.get('https://en.wikipedia.org/wiki/Taylor_Swift')
 
-        search_query = driver.find_element(By.NAME, 'q')
-        search_query.send_keys(self.name + ' wikipedia')
-        search_query.send_keys(Keys.RETURN)
+        # search_query = driver.find_element(By.NAME, 'q')
+        # search_query.send_keys(self.name + ' wikipedia')
+        # search_query.send_keys(Keys.RETURN)
 
-        wiki_url = driver.find_element(By.CLASS_NAME, 'iUh30').text.replace(' › ', '/')
-        driver.get(wiki_url)
+        # wiki_url = driver.find_element(By.CLASS_NAME, 'iUh30').text.replace(' › ', '/')
+        # driver.get(wiki_url)
 
         self.wiki_report = driver.find_element(By.XPATH, '//*[@id="mw-content-text"]/div[1]/p[3]').text
 
     def get_wiki_report(self):
         return self.wiki_report
 
-    def create_db(self):
+    def set_concert_report(self):
+        driver = webdriver.Chrome()
+        driver.get('https://www.taylorswift.com/tour/')
+
+        query = driver.find_element(By.CLASS_NAME, 'view-events')
+        elements = query.find_elements(By.XPATH, './/*')
+
+        out = []
+        for elem in elements:
+            data = elem.text.split('\n')
+            if len(data) == 5:
+                print(data)
+                concert_data = {'date': data[0], 'stadium': data[1], 'place': data[2]}
+                out.append(concert_data)
+
+        self.concert_upcoming = out
+
+    def get_concert_report(self):
+        return self.concert_upcoming
+
+    def create_json(self):
         print(self.name)
-
-        sql_code = """CREATE TABLE ARTIST(
-  NAME VARCHAR2(50),
-  INFO VARCHAR2(500),
-  CONSTRAINT PK_ARTIST PRIMARY KEY (NAME)
-);
-
-CREATE TABLE CONCERT(
-    CITY VARCHAR2(50),
-    COUNTRY VARCHAR2(50),
-    VENUE VARCHAR2(50),
-    TICKET_SOLD INT(5),
-    TICKET_AVAILABLE INT(5),
-    REVENUE FLOAT(4),
-    TOUR_NAME VARCHAR2(50)
-);
-"""
-
-        # Format and save SQL code to a file
-        formatted_sql = sqlparse.format(sql_code)
-        with open('my_sql_file.sql', 'w') as sql_file:
-            sql_file.write(formatted_sql)
 
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import InfoArtistClass as iac
 
-# test = iac.InfoArtist()
-# test.set_name("Taylor Swift")
+test = iac.InfoArtist()
+test.set_name("Taylor Swift")
 # test.set_wiki_report()
 # print(test.get_wiki_report())
-# test.create_db()
-print("hola")
+# test.set_concert_report()
+# print(test.get_concert_report())
+
+# test.create_json()
